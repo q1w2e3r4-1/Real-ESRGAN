a296a8c3a00a8bbc277f63d6b9d1eb74a5eda560
q1w2e3r4 <211250228@smail.nju.edu.cn>
2024-10-07 21:23:59 +0800

    add some training yaml

---
+++ example.bat
@@ -0,0 +1,2 @@
+python inference_realesrgan.py -n RealESRGAN_x4plus -i inputs/Chase_20220515_1644.png --outscale 2 --fp32
+python inference_realesrgan.py -n RealESRGAN_x4plus -i inputs/dog.jpg --outscale 2 --fp32
